4dd217d70ec6f96eb89509effa7945253f6bba46
Clara Bessa <clarabessa@outlook.com>
2025-04-14 20:14:38 -0300

    formatando a moeda

--- scripts.js
+++ scripts.js
@@ -1,7 +1,24 @@
 // seleciona os elementos do formunlário
 const amount = document.getElementById("amount")
 
+// captura o evento de input para formatar o valor
 amount.oninput = () => { 
+    // obtém o valor atual do input e remove os caracteres nao numericos
     let value = amount.value.replace(/\D/g, "")
-    amount.value = value
+
+    // transformar o valor em centavos
+    value = Number()/100
+
+    // atualiza o valor do input
+    amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value){
+    // formata o valor para brl
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    })
+
+    return value
 }
